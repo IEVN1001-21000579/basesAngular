c76c7a3dc1822b8e79cefc90eff9119c7e453538
cesar uriel isaac <21000579@alumnos.utleon.edu.mx>
2024-09-17 21:08:08 -0600

    first commit del martes 17

---
+++ src/app/product/product.ist/product.ist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product.ist',
+  templateUrl: './product.ist.component.html',
+  styleUrl: './product.ist.component.css'
+})
+export class ProductIstComponent {
+
+}
