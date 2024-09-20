30a8c8297509c6c33e5ee5238fd814f375c3688d
cesar uriel isaac <21000579@alumnos.utleon.edu.mx>
2024-09-19 21:01:13 -0600

    Segundo commit jueves 19 botón

--- src/app/product/product.ist/product.ist.component.ts
+++ src/app/product/product.ist/product.ist.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-product.ist',
+  selector: 'app-product-ist',
   templateUrl: './product.ist.component.html',
   styleUrl: './product.ist.component.css'
 })
 export class ProductIstComponent {
 
+  imageWidth:number=180;
+  imageMargin:number=8;
+  muestraImg:boolean=true;
+  mostrarImagen():void{
+    this.muestraImg=!this.muestraImg
+  }
+
+  productos:any[]=[
+    {
+      "ProductoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"2 puertas",
+      "Year":"2024",
+      "Precio":12000,
+      "Color":"Blanco",
+      "ImagenUrl":"https://th.bing.com/th/id/OIP.8LYqFD_JM_A03E6M3czP1gAAAA?rs=1&pid=ImgDetMain",
+      "Marca":"si"
+    },
+    {
+      "ProductoId":2,
+      "Modelo":"Tesla",
+      "Descripcion":"4 puertas",
+      "Year":"2025",
+      "Precio":120000,
+      "Color":"Gris",
+      "ImagenUrl":"https://i.ytimg.com/vi/4VxiZx4OIuY/maxresdefault.jpg",
+      "Marca":"si"
+    },
+    {
+      "ProductoId":3,
+      "Modelo":"Bocho",
+      "Descripcion":"1 puerta",
+      "Year":"2000",
+      "Precio":55,
+      "Color":"Caca",
+      "ImagenUrl":"https://i.kinja-img.com/gawker-media/image/upload/s--zKXRyv-o--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/18nbgrzzzl56cjpg.jpg",
+      "Marca":"si"
+    }
+  ]
 }
